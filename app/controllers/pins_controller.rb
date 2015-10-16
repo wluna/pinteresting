bf1fe6dfc0caee15de8151167ef9fd03d66de0e3
wluna <william.luna@tufts.edu>
2015-10-16 17:34:50 -0500

    associations fixed

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,58 @@
+class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  respond_to :html
+
+  def index
+    @pins = Pin.all
+    respond_with(@pins)
+  end
+
+  def show
+    respond_with(@pin)
+  end
+
+  def new
+    #@pin = Pin.new
+    # to enable the pin automatically being assigned to the user
+   @pin = current_user.pins.build
+    respond_with(@pin)
+  end
+
+  def edit
+  end
+
+  def create
+
+    #@pin = Pin.new(pin_params)
+    #to enable pins being automatically assigned to users
+    @pin = current_user.pins.build
+    @pin.save
+    respond_with(@pin)
+  end
+
+  def update
+    @pin.update(pin_params)
+    respond_with(@pin)
+  end
+
+  def destroy
+    @pin.destroy
+    respond_with(@pin)
+  end
+
+  private
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path if @pin.nil?
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014220616) do
+ActiveRecord::Schema.define(version: 20151015220907) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
